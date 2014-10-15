58d6d438bddf6b3e46f459277f35972898274539
Khadgar <daniel.szaniszlo@gmail.com>
2014-10-15 23:45:27 +0200

    Working vote result handling
    On /result now you can see a table with the last 3 vote results
    decision.

--- decision.js
+++ decision.js
@@ -2,7 +2,7 @@ module.exports = function (VoteResults, Votes, io) {
 	//timer begin
 	//todo: ezt a logikat at kellene rakni masik fajlba.
 	//az utolso lezart szavas eredmenyet el lehetne tarolni adatbazisban igy barmelyik oldalon lekerdezheto lenne
-	var sec = 20;
+	var sec = 120;
 	var timeout = setTimeout(function () {}, sec * 1000);
 
 	setInterval(function () {
@@ -25,7 +25,7 @@ module.exports = function (VoteResults, Votes, io) {
 
 	//itt definialom a a callback fuggvenyt parameterrel. ha ezt nem tennem meg akkor a insertResults vegen a callback(votes) -nak nem lenne ertelme
 	function query(mydata) {
-		io.emit('lastres', mydata);
+		//io.emit('lastres', mydata); //mar van tablazatom nincs erre szukseg
 		console.log(mydata);
 	}
 	
@@ -49,32 +49,40 @@ module.exports = function (VoteResults, Votes, io) {
 			}, function (err, vote) {
 				votes = vote.id + ',' + vote.option1 + ',' + vote.option2 + ',' + vote.option3;
 				callback(votes);
-		
-		//insert the results into VoteResults collection
+				
+				//decision making simple majority decision, later weighted decision
+				var finaldecision = null;
+				if(vote.option1>vote.option2 && vote.option1>vote.option3){
+					finaldecision = 'Option 1';
+				}else if(vote.option2>vote.option1 && vote.option2>vote.option3){
+					finaldecision = 'Option 2';
+				}else if(vote.option3>vote.option1 && vote.option3>vote.option2){
+					finaldecision = 'Option 3';
+				}else{
+					finaldecision = 'Can not decide.';
+				}
+				
+				//insert the results into VoteResults collection
 				var newresults = {
 					vote_id : vote.id,
-					results : vote.option1 + ',' + vote.option2 + ',' + vote.option3
+					results : vote.option1 + ',' + vote.option2 + ',' + vote.option3+','+finaldecision
 				};
 				var voteresult = new VoteResults(newresults);
 
 				voteresult.save();
 		
-		//reset the vote
+				//reset the vote and tell the clients: the current vote has closed
 				Votes.findOne({
 					id : '1'
 				}, function (error, vote) {
 					vote.option1 = 0;
 					vote.option2 = 0;
 					vote.option3 = 0;
+					io.emit('votes', vote.option1+','+vote.option2+','+vote.option3);
+					io.emit('voteclosed','lezarulta szavazas most kerd az eredmenyt');
 					vote.save();
 				});
-				
-				//utolso 3 lezart szavazas eredmenye. a kliens oldalon majd meg kell jeleniteni a dontessel egyutt.
-				VoteResults.find({}, 'vote_id results').sort({'_id': -1}).limit(3).exec(function (err, result) {
-					io.emit('allresults', result);
-					console.log(result);
-				});
-				
+
 			});
 		});
 	};
@@ -83,12 +91,27 @@ module.exports = function (VoteResults, Votes, io) {
 	
 	function getAllResults(callback) {
 		process.nextTick(function () {
-		
 			VoteResults.find({}, 'vote_id results').sort({'_id': -1}).limit(3).exec(function (err, result) {
 				callback(result);
 			});
 			
 		});
 	};
+	
+	//result.html betoltesekor a keresere elkuldi a valaszt
+	io.on('connection', function (socket) {
+		console.log('a user connected');
 
+		socket.on('message2', function (msg) {
+			console.log('message: ' + msg);
+			VoteResults.find({}, 'vote_id results').sort({'_id': -1}).limit(3).exec(function (err, result) {
+				io.emit('allresults', result);
+			});
+		});
+		
+		socket.on('disconnect', function () {
+			console.log('user disconnected');
+		});
+	});
+	
 }
